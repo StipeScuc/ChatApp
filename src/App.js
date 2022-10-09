de1f8f4f12710417af62daab8ce7d29d68253946
Stip Sculija <stipe.scuc@gmail.com>
2022-10-09 18:03:16 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Messages from "./components/Messages";
+
+const names = ['Ivana', 'Valentina', 'Mirko', 'Stipe', 'Maja', 'Petar', 'Ana', 'Zlatko', 'Anita', 'Josip', 'Bojan', 'Loreta', 'Andrea', 'Mihael', 'Mihaela', 'Lea', 'Leo', 'Tea', 'David', 'Milan', 'Kristina', 'Adriano', 'Goran', 'Ivan', 'Ivica', 'Karla', 'Ivo', 'Jela', 'Lucija', 'Marijana', ];
+
+const surnames = ['Ivanic', 'Valentin', 'Markic', 'Stipetic', 'Majic', 'Petric', 'Anic', 'Zlatic', 'Anic', 'Josipovic', 'Bojanic', 'Loric', 'Mihalic', 'Lesic', 'milanovic', 'Gorcic', 'Jelic', 'Lucijanic', 'Ivic', 'Karlic', 'Sipic', 'Martic', 'Bacic', 'Baric', 'Sladic'];
+
+const fullName = names[Math.floor(Math.random() * names.length)] + " " + surnames[Math.floor(Math.random() * surnames.length)];
+
+const color = () => {return '#' + Math.floor(Math.random()*16777215).toString(16)};
+
+const drone = new window.Scaledrone("qp0gEkDFaI6eWuaz", {
+  data: {name: fullName, color: color()}
+});
+const room = drone.subscribe("observable-chatRoom");
 
 function App() {
+  const [text, setText] = useState("");
+  const [messages, setMessages] = useState([])
+
+  const onChange = (e) => {
+    setText(e.target.value)
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setText("");
+    drone.publish({
+      room: "observable-chatRoom",
+      message: text
+    });
+  }
+
+  useEffect(() => {
+    drone.on('open', error => {
+      if (error) {
+        return console.log(error);
+      }
+    });
+
+    room.on('open', error => {
+      if (error) {
+        return console.error(error)
+      }else {
+        console.log('Connected')
+      }
+    });
+
+    room.on('data', (data, member) => {
+      const message = messages;
+      message.push({text: data, member, user: fullName});
+      setMessages([...message])
+    });
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main">
+      <h1>ChatApp</h1>
+      <Messages messages={messages}/>
+      <form onSubmit={(e) => onSubmit(e)}>
+                <input
+                    onChange={(e) => onChange(e)}
+                    value={text}
+                    type="text"
+                    placeholder="Enter your message"
+                    autoFocus={true}
+                />
+                <button><i className="fa fa-send"></i></button>
+            </form>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Messages.js
@@ -0,0 +1,27 @@
+function Messages(props) {
+    const showMessage = (message) => {
+        const {member, text, user} = message;
+        const currentMember = member.clientData.name === user;
+        const className = currentMember ? 'curent' : 'other';
+        const memberColor = currentMember ? {borderRight: `solid ${member.clientData.color} 7px`} : {borderLeft: `solid ${member.clientData.color} 7px`};
+
+        return (
+            <li key={member.clientData.color} className={className}>
+                <div className="message">
+                    <p className="name"><b>{member.clientData.name}</b></p>
+                    <p className="text" style={memberColor}>{text}</p>
+                </div>
+            </li>
+        )
+    }
+
+    return (
+        <div id="messages">
+            <ul>
+                {props.messages.map((m) => showMessage(m))}
+            </ul>
+        </div>
+    )
+}
+
+export default Messages;
